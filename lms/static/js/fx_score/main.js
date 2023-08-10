9b17164819e5e28c64d679c6f4a82d3f470fc3f2
quannguyen <hdragon1501@gmail.com>
2023-08-10 16:45:36 +0700

    Add pages to score module

---
+++ lms/static/js/fx_score/main.js
@@ -0,0 +1,25 @@
+
+function showViewPage(){
+
+    const main_page = document.getElementById("main-page");
+    main_page.style.display = 'none';
+
+    const view_page = document.getElementById("view-page");
+    view_page.style.display = 'block';
+}
+
+function showSubmitFilePage(){
+    const main_page = document.getElementById("main-page");
+    main_page.style.display = 'none';
+
+    const submit_file_page = document.getElementById("submit-file-page");
+    submit_file_page.style.display = 'block';
+}
+
+function showBookPage(){
+    const main_page = document.getElementById("main-page");
+    main_page.style.display = 'none';
+
+    const book_page = document.getElementById("book-page");
+    book_page.style.display = 'block';
+}
